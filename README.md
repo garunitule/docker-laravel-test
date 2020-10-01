f39d386268808c648d588be139a56c3bc0b40f82
garunitule <youtaiguhe@gmail.com>
2020-10-01 23:04:23 +0900

    install react-markdown

--- README.md
+++ README.md
@@ -2,11 +2,16 @@
 
 docker-compose buildしたら以下のコマンドを実行してlaravelプロジェクトを作成して、reactをのせる
 
+## laravelプロジェクト作成
 docker exec -it laravel_app bash
 cd /var/www/html
 composer create-project "laravel/laravel=~7.0" --prefer-dist laravelapp
 
+## reactを使えるように
 cd /var/www/html/laravelapp
 composer require laravel/ui:^2.4
 php artisan ui react
 npm install && npm run dev
+
+## マークダウンをHTMLにパースするライブラリインストール
+npm install --save react-markdown
--- src/laravelapp/resources/js/components/Example.js
+++ src/laravelapp/resources/js/components/Example.js
@@ -1,24 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function Example() {
-    return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
-
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
-
-export default Example;
+import ReactMarkdown from 'react-markdown';
 
 if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
+    const input = '# This is a header\n\n## This is a second header\n\nAnd this is a paragraph';
+    ReactDOM.render(<ReactMarkdown source={input} />, document.getElementById('example'));
 }
