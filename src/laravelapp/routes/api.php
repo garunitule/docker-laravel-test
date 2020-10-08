5768502123ac3079e39b3bdc242cf51701f7c117
garunitule <youtaiguhe@gmail.com>
2020-10-08 21:30:43 +0900

    ルーティング修正漏れ

--- src/laravelapp/routes/api.php
+++ src/laravelapp/routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'api'], function(){
+    Route::get('/articles/{id}', 'Api\ArticlesController@get');
+    Route::get('/articles', 'Api\ArticlesController@getAll');
 });
